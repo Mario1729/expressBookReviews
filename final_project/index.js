55530eaa1556b6358993cca0ade0145bc9c18c3b
Mario <Mario1729@github.com>
2025-08-06 09:26:19 -0400

    added functionality to general.js

--- final_project/index.js
+++ final_project/index.js
@@ -7,36 +7,6 @@ const genl_routes = require('./router/general.js').general;
 const app = express();
 app.use(express.json());
 
-let users = []
-
-// Check if a user with the given username already exists
-const doesExist = (username) => {
-    // Filter the users array for any user with the same username
-    let userswithsamename = users.filter((user) => {
-        return user.username === username;
-    });
-    // Return true if any user with the same username is found, otherwise false
-    if (userswithsamename.length > 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-// Check if the user with the given username and password exists
-const authenticatedUser = (username, password) => {
-    // Filter the users array for any user with the same username and password
-    let validusers = users.filter((user) => {
-        return (user.username === username && user.password === password);
-    });
-    // Return true if any valid user is found, otherwise false
-    if (validusers.length > 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
@@ -56,7 +26,6 @@ app.use("/customer/auth/*", function auth(req,res,next){
     } else {
         return res.status(403).json({ message: "User not logged in" });
     }
-
 });
  
 const PORT =5000;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,32 +12,62 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books,null,4));
+  // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = parseInt(req.params.isbn);
+    let book = books[isbn];
+    if (book)
+        res.send(book);
+    else
+        res.send('This ISBN \'' + isbn + '\' is not available.');
+    // return res.status(300).json({message: "Yet to be implemented"});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    const booksArray = Object.values(books);
+    let book = booksArray.filter((item)=>item.author === author);
+    if (book.length > 0)
+        res.send(book);
+    else
+        res.send('This author \'' + author + '\' is not available.');
+    // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    const booksArray = Object.values(books);
+    let book = booksArray.filter((item)=>item.title === title);
+    if (book.length > 0)
+        res.send(book);
+    else
+        res.send('This title \'' + title + '\' is not available.');
+    // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const isbn = parseInt(req.params.isbn);
+
+    // 1. Get the object
+    const book = books[isbn];
+    if (book){
+        // 2. Convert the JavaScript object to a JSON string (optional, but common for JSON)
+        const jsonString = JSON.stringify(book);
+
+        // 3. You can then parse it back to a JavaScript object if needed
+        const parsedObject = JSON.parse(jsonString);
+
+        return res.send(parsedObject.reviews);
+    }
+    return res.send('This ISBN \'' + isbn + '\' is not available.');
+    // return res.status(300).json({message: "Yet to be implemented"});
+ });
 
 module.exports.general = public_users;
